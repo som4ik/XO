a52fac8d8a24c4376274bef8b7dcaaf95b90b9df
som4ik <somar_melhem@hotmail.com>
2016-12-03 23:18:55 +0300

    add player model

---
+++ spec/models/player_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Player, type: :model do
+  describe "Player validation" do
+    it "has valid factory" do
+      player = FactoryGirl.create(:player)
+    end
+    it { should validate_presence_of(:name) }
+  end
+end
